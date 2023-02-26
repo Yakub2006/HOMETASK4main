4031054bca1e91b41c955c09931d43e409e77e1e
Yakub2006 <114349408+Yakub2006@users.noreply.github.com>
2023-02-26 13:41:46 +0300

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Task2 {
+import java.util.Scanner;
+
+public class Task2 {
+  public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int[][][] arr = new int[3][3][3];
+        for(int i = 0;i < 3;i++){
+            for(int j = 0;j < 3;j++){
+                for(int n = 0;n < 3;n++){
+                    arr[i][j][n] =(int)(Math.random()*10);
+                    System.out.print(arr[i][j][n] + " ");
+                    arr[i][j][n] += x;
+                    System.out.print("||- result " + arr[i][j][n] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
 }
