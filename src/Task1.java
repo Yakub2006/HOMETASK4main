7428cd5fa11a9c171529eead07923b02ed018541
Yakub2006 <114349408+Yakub2006@users.noreply.github.com>
2023-02-26 13:40:22 +0300

    Update Task1.java

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,19 @@
 public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+   public static void main(String[] args) {
+        String[][] arr = new String[8][8];
+        for(int i = 0;i<8;i++){
+            for(int j = 0;j<8;j++){
+                if(i%2==0 && j%2==0){
+                    arr[i][j] = "W";
+                }else if(i%2==1 && j%2==1){
+                    arr[i][j] = "W";
+                }else {
+                    arr[i][j] = "B";
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
+    
 }
